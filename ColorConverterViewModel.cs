31d46a98d5fbd9d3890d3dd49aeae3abf0e4e5d8
kubpi <jakub.pietyszuk@gmail.com>
2023-11-06 12:03:28 +0100

    jest kostka ale nwm czy to o to chodzi

--- ColorConverterViewModel.cs
+++ ColorConverterViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using Brush = System.Windows.Media.Brush;
 
 namespace kolory
 {
@@ -14,6 +19,16 @@ namespace kolory
     {
         private ColorModel _color;
         private ICommand _convertCommand;
+        private DiffuseMaterial _gradientMaterial;
+        public DiffuseMaterial GradientMaterial
+        {
+            get { return _gradientMaterial; }
+            private set
+            {
+                _gradientMaterial = value;
+                OnPropertyChanged(nameof(GradientMaterial));
+            }
+        }
 
         public ColorModel Color
         {
@@ -34,6 +49,7 @@ namespace kolory
         {
             _color = new ColorModel();
             _color.PropertyChanged += ColorModel_PropertyChanged;
+            GradientMaterial = CreateGradientMaterial();
         }
 
         private void ColorModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -56,9 +72,40 @@ namespace kolory
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        public DiffuseMaterial CreateGradientMaterial()
+        {
+            // Your algorithm to create a System.Drawing.Bitmap
+            var bitmap = Algorithm.Gradient();
 
+            // Convert the System.Drawing.Bitmap to a BitmapSource
+            var bitmapSource = ConvertBitmapToBitmapSource(bitmap);
 
+            // Create a brush with the BitmapSource
+            var brush = new ImageBrush(bitmapSource)
+            {
+                ViewportUnits = BrushMappingMode.Absolute,
+                TileMode = TileMode.Tile,
+                Stretch = Stretch.Fill
+            };
 
+            // Create and return the material with the brush
+            return new DiffuseMaterial(brush);
+        }
+
+        private BitmapSource ConvertBitmapToBitmapSource(Bitmap bitmap)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                memoryStream.Position = 0;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
     }
 
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,12 +21,18 @@ namespace kolory
     /// </summary>
     public partial class MainWindow : Window
     {
-       
+        public DiffuseMaterial GradientMaterial { get; set; }
         public MainWindow()
         {
             InitializeComponent();
             // Inicjowanie ViewModeli
-            DataContext = new ColorConverterViewModel();
+
+            // Set the DataContext for the window to your ViewModel
+            var viewModel = new ColorConverterViewModel();
+            DataContext = viewModel;
+
+            // Assign the gradient material to the cube
+            GradientMaterial = viewModel.GradientMaterial;
 
         }
 
